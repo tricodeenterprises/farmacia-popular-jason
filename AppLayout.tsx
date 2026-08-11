293eaeade10a47e1a7227c328b5b62a3731a9036
Jason <jtrallii@live.com>
2026-08-11 12:00:07 -0300

    remoção de dependencias e codigos não usados

--- AppLayout.tsx
+++ AppLayout.tsx
@@ -1,6 +1,6 @@
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap');
 
-#lovable-badge { display: none !important; }
+
 
 @tailwind base;
 @tailwind components;
